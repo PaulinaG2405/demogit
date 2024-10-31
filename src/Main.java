705b278ef88972e91ec2d818ca9aab228d84d5ec
PaulinaG2405 <paoguzmantejada@gmail.com>
2024-10-31 08:21:22 -0500

    se crea vehiculo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Estamos practicando SCV");
+        String name = "John Doe";
+        int age = 25;
+        boolean status = true;
 
     }
 }
